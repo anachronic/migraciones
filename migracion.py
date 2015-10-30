fab925d4f26d3322bf1a7f13e9d00c1ca78e4a70
Nicolas Salas V <nikosalas@gmail.com>
2015-10-30 20:44:23 -0300

    Initial commit

---
+++ migracion.py
@@ -0,0 +1,30 @@
+import datetime
+import time
+import MySQLdb as mdb
+
+con = mdb.connect('localhost', 'root', 'password', 'zenergy')
+out = mdb.connect('localhost', 'root', 'password', 'test')
+
+insert_query = "INSERT INTO datos(id, timestamp, valor, dispositivo_id, indicador_id) VALUES (DEFAULT, %s, %s, %s, %s)"
+
+cur = con.cursor()
+cur.execute("SELECT * from datos where id_dispositivo=7000 and instante is not NULL and valor is not NULL ")
+
+cout = out.cursor()
+
+rows = cur.fetchall()
+
+for row in rows:
+    timestamp = row[1]
+    valor = row[4]
+    if timestamp is None or valor is None:
+        continue
+    cout.execute(insert_query, (timestamp, valor, 1, 1))
+
+out.commit()
+
+if con:
+    con.close()
+
+if out:
+    out.close()
